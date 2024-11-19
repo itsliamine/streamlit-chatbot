031e4096867da00dbb5229b5fae7cf9860827b9c
Farès Aoudia <fares.aoudia0@gmail.com>
2024-11-19 01:07:11 +0100

    Main files

---
+++ app.py
@@ -0,0 +1,4 @@
+from src.ui.interface import ui
+
+if __name__ == "__main__":
+    ui()
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from src.data_preprocessing.preprocess import preprocess_csv
+from src.data_preprocessing.embeddings import generate_embeddings, upload_embeddings
+import json
 
 raw_filepath = "data/bdd.csv"
-preprocessed_filepath = "data/preprocessed.csv"
+processed_filepath = "data/preprocessed.csv"
 
 if __name__ == "__main__":
-    preprocess_csv(raw_filepath, preprocessed_filepath)
-    print("Cleaned data saved.")
+	preprocess_csv(raw_filepath, processed_filepath)
+	embeds = generate_embeddings(processed_filepath)
+	upload_embeddings(
+		embeddings=embeds
+	)
