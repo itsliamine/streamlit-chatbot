f6def47312b36af700e1a1b2db2862ad5205200e
Farès Aoudia <fares.aoudia0@gmail.com>
2024-11-21 01:22:59 +0100

    Switched to Azure OpenAI

--- src/chatbot/retrieval.py
+++ src/chatbot/retrieval.py
@@ -1,37 +1,48 @@
 from src.chatbot.prompt import prompt_template
 
-from langchain.llms import OpenAI
-from langchain.chains import LLMChain
+from langchain_community.llms import AzureOpenAI
 from langchain.chains import RetrievalQA
 from src.pinecone.init import pinecone_init
-from langchain.vectorstores import Pinecone
-from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import Pinecone
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 def retrieve():
-	pc = pinecone_init()
-	index = pc.Index("law-test")
 
-	embeddings_function = OpenAIEmbeddings(
-		model="text-embedding-3-small"
-	)
+    index = pinecone_init("law-test")
 
-	vector_store = Pinecone(
-		index, 
-		embeddings_function.embed_query, 
-		"text"
-	)
- 
-	llm: OpenAI = OpenAI(
-		temperature=0,
-	)
+    embeddings_function = AzureOpenAIEmbeddings(
+        model='text-embedding-ada-002',
+    )
 
-	qa: RetrievalQA = RetrievalQA.from_chain_type(
-		llm=llm,
-		chain_type="stuff", 
-		retriever=vector_store.as_retriever(),
-  		chain_type_kwargs={
-			"prompt": prompt_template
-		}
-	)
- 
-	return qa
+    vector_store = Pinecone(
+        index,
+        embeddings_function,
+        "text",
+    )
+
+    llm: AzureOpenAI = AzureChatOpenAI(
+        temperature=0,
+        azure_endpoint=os.getenv('AZURE_OPENAI_ENDPOINT'),
+        openai_api_key=os.getenv('AZURE_OPENAI_API_KEY'),
+        azure_deployment=os.getenv('LLM_DEPLOYMENT')
+    )
+
+    qa: RetrievalQA = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(
+            search_kwargs={
+                "k": 3,
+            }),
+        chain_type_kwargs={
+            "prompt": prompt_template
+        }
+    )
+
+    return qa
--- src/data_preprocessing/embeddings.py
+++ src/data_preprocessing/embeddings.py
@@ -3,42 +3,72 @@ import json
 import pandas as pd
 from dotenv import load_dotenv
 
+from tqdm import tqdm
+
 from src.pinecone.init import create_db
 from src.pinecone.upsert import upsert_vectors
 
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import AzureOpenAIEmbeddings
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
 
 def generate_embeddings(input_csv):
-	df = pd.read_csv(input_csv)
-	text_data = df["Réponse écrite de DP"].tolist()
- 
-	embeddings_function = OpenAIEmbeddings(
-		model="text-embedding-3-small"
-	)
- 
-	embeddings = []
-	for idx, text in enumerate(text_data):
-		embedding = embeddings_function.embed_query(text)
-		embeddings.append((str(idx), embedding, {"text": text}))
+    df = pd.read_csv(input_csv)
+    answers = df["answers"].tolist()
+    questions = df["questions"].tolist()
 
-	with open('embeddings.json', 'w') as f:
-		json.dump(embeddings, f)
+    embeddings_function = AzureOpenAIEmbeddings(
+        model='text-embedding-ada-002'
+    )
 
-	return embeddings
-	
-	
+    embeddings = []
+    for idx, (question, answer) in enumerate(
+        tqdm(
+            zip(questions, answers),
+            total=len(questions),
+            desc="Embedding Progress"
+        )
+    ):
+        answer_embedding = embeddings_function.embed_query(answer)
+        embeddings.append((
+            f"a_{idx}",
+            answer_embedding,
+            {
+                "text": answer,
+                "type": "answer",
+                "question": question,
+                "pair_id": idx
+            }
+        ))
+
+        question_embedding = embeddings_function.embed_query(question)
+        embeddings.append((
+            f"q_{idx}",
+            question_embedding,
+            {
+                "text": question,
+                "type": "question",
+                "answer": answer,
+                "pair_id": idx
+            }
+        ))
+
+    with open('embeddings.json', 'w') as f:
+        json.dump(embeddings, f)
+
+    return embeddings
+
+
 def upload_embeddings(embeddings):
-	index_name = "law-test"
- 
-	create_db(
-		index_name=index_name,
-		dimension=len(embeddings[0][1])
-	)
- 
-	upsert_vectors(
-		index_name=index_name, 
-		embeddings=embeddings
-	)
+    index_name = "law-test"
+
+    create_db(
+        index_name=index_name,
+        dimension=len(embeddings[0][1])
+    )
+
+    upsert_vectors(
+        index_name=index_name,
+        embeddings=embeddings
+    )
