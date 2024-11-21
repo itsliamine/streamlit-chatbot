19a5fe2ae0a76f805ce2f7c452539446b0581bf7
gbenard <gwendal.benard@gmail.com>
2024-11-21 20:36:30 +0100

    doc

---
+++ Api/Documentation.mk
@@ -0,0 +1,18 @@
+# Documentation de l'API Flask
+
+## Description
+Cette API permet de gérer la connection entre le Front-end et le backend avec des fonctionnalités pour :
+- Faire une demande de prompt.
+- Dialoguer avec l'ia.
+
+---
+
+## Installation
+
+### Prérequis
+- Python 3.x
+- Pip (gestionnaire de paquets Python)
+
+### Installation des dépendances
+```bash
+pip install flask
